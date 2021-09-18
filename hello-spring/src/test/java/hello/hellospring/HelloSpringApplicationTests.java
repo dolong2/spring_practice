eb1ef518e39ce67134363fd22a220d8b39c8c605
jjo <k01066624566@gmail.com>
2021-09-18 19:10:08 +0900

    test

--- hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
+++ hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -1,13 +1,30 @@
 package hello.hellospring;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@Transactional
 class HelloSpringApplicationTests {
-
+	@Autowired
+	MemberService memberService;
+	@Autowired
+	MemberRepository memberRepository;
 	@Test
-	void contextLoads() {
+	void 회원가입() {
+		Member member = new Member();
+		member.setName("spring");
+		Long saveId=memberService.join(member);
+		Member findMember=memberService.findOne(saveId).get();
+		assertThat(member.getName()).isEqualTo(findMember.getName());
 	}
 
 }
