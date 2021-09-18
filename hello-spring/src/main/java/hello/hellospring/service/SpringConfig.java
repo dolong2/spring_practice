dca0e9f83d0400f01746b877706d6a452311d05f
jjo <k01066624566@gmail.com>
2021-09-18 20:57:10 +0900

    add jdbctemplate

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,13 +1,10 @@
 package hello.hellospring.service;
-
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 public class SpringConfig {
