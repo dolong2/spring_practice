9db95112634a2804342b052ff71ad9e9a8f043a8
jjo <k01066624566@gmail.com>
2021-09-19 19:49:06 +0900

    add Spring data JPA

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,4 @@
 package hello.hellospring.service;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,21 +11,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class SpringConfig {
     private DataSource dataSource;
     private EntityManager em;
-
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em){
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository){
         this.dataSource=dataSource;
         this.em = em;
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());//생성자 주입
+        return new MemberService(memberRepository);//생성자 주입
     }
-    @Bean
-    public MemberRepository memberRepository(){
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);//생성자 주입
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);//생성자 주입
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 }//자바 코드로 스프링빈에 등록하기
